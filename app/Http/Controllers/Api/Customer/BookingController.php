84da7b3e0152f2b0630d4858b1709ed8daf1f552
gilbert <gilbertsibajene7@gmail.com>
2022-02-07 13:25:42 +0200

    Added Payments

--- app/Http/Controllers/Api/Customer/BookingController.php
+++ app/Http/Controllers/Api/Customer/BookingController.php
@@ -76,7 +76,7 @@ class BookingController extends Controller
                 return response()->json($paid->responseMessage,400);
             }
         }catch(Exception $e){
-            return response()->json($e,400);
+            return response()->json($e->getMessage(),400);
         }
 
     }
